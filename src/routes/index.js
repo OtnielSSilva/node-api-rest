321817dae65a27ccfbfd5dabe7194bec9de5abe0
OtnielSSilva <otniel.dss04@gmail.com>
2022-12-09 12:02:23 -0400

    first commit

---
+++ src/routes/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+// import router from "./livrosRoutes.js";
+import livros from './livrosRoutes.js';
+import autores from './autoresRoutes.js'
+
+const routes = (app) => {
+    app.route('/').get((req, res) => {
+        res.status(200).send({titulo: 'Curso de node'});
+    });
+
+    app.use(
+        express.json(),
+        livros,
+        autores
+    );
+};
+
+export default routes;
